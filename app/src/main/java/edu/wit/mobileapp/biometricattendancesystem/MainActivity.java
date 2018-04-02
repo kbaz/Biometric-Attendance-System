7a6de820cc436eb51984a33061bc74d03f47f735
Kyle <kbasler340@gmail.com>
2018-04-01 23:24:43 -0400

    buttons and toast message

--- app/src/main/java/edu/wit/mobileapp/biometricattendancesystem/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/biometricattendancesystem/MainActivity.java
@@ -2,12 +2,33 @@ package edu.wit.mobileapp.biometricattendancesystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addStudent, captureAttendance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addStudent = findViewById(R.id.addStudent);//get id of button 1
+        captureAttendance = findViewById(R.id.captureAttendance);//get id of button 2
+
+        addStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Add Student", Toast.LENGTH_LONG).show();//display the text of button1
+            }
+        });
+        captureAttendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Capture Attendance", Toast.LENGTH_LONG).show();//display the text of button2
+            }
+        });
     }
 }
